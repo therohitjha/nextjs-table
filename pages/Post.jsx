e844e15dffdcea6054d214fbe2f136791ab6c5e4
Rohit Jha <therohitjha@gmail.com>
2020-11-07 22:51:17 +0530

    first commit

---
+++ pages/Post.jsx
@@ -0,0 +1,30 @@
+import { withRouter } from 'next/router'
+import Link from 'next/link'
+function Post({post}){
+
+    return (<>
+    {Object.keys(post).length && <Link href='/' ><div className={'back'}>Back</div></Link>}
+    { Object.keys(post).length ? <div className={'container'}>
+          <img
+          alt=""
+          src={`https://robohash.org/${post.id}?200x200`}
+          width={200}
+          height={200}
+        />
+        <div className={'name padding'}>{post.name}</div>
+        <div className={'username padding'}>Username :- @{post.username}</div>
+        <div className={'padding'}>Email :- {post.email}</div>
+        <div className={'padding'}>Website :- <a href={post.website}>{post.website}</a></div>
+        <div className={'padding'}>Phone :- {post.phone}</div>
+        <div className={'padding'}>Company :- {post.company.name}</div>
+        <div className={'padding'}>City :- {post.address.city}</div>
+    </div> : 'No Data'}</>)
+}
+
+export async function getServerSideProps({query:{id}}) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const post = await res.json()
+    return { props: { post } }
+  }
+
+export default withRouter(Post)
---
+++ pages/Table.jsx
@@ -0,0 +1,56 @@
+import React,{useEffect,useState} from 'react'
+import MaUTable from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import { useTable } from 'react-table'
+import { useRouter } from 'next/router'
+
+export default function Table({ columns, data }) {
+    const { getTableProps, headerGroups, rows, prepareRow } = useTable({
+      columns,
+      data,
+    })
+    const router = useRouter()
+  
+const handlePostData=(data)=>{
+router.push({
+    pathname: '/Post',
+    query: data.row.values
+})
+}
+    
+    return (
+      <MaUTable {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map(headerGroup => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <TableCell {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <TableCell style={{cursor:'pointer'}} {...cell.getCellProps()} onClick={(e)=>handlePostData(cell)}>
+                   {cell.render('Cell')}
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </MaUTable>
+    )
+  }
+  
